a34c5a9a7290a5345b347903911098af7a8e0b12
hcustod <h.custodio.dev@gmail.com>
2026-03-21 14:06:40 -0400

    Add: Lab 7 complete

--- Lab07_101497015_COMP3133/exercise1/myapp/routes/users.js
+++ Lab07_101497015_COMP3133/exercise1/myapp/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/* POST users */ 
+router.post('/', function(req, res, next) {
+  console.log('First Name : ', req.body.firstName);
+  console.log('Last Name : ', req.body.lastName);
+  res.send('POST received!');
 });
 
 module.exports = router;
